d8ea41777ca8dd61e40196e7683efe36d0109cbe
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-28 11:57:10 +0100

    Add project files.

---
+++ C Uppgift 5.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace Uppgift_5._3
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string[] namn = { "Ludwig", "Johan", "Zacharias", "Kevin", "Ameen" };
+
+            int index = 0;
+
+            for (int i = 0; i < namn.Length; i++)
+            {
+                if (namn[i].CompareTo(namn[index]) < 0)
+                {
+                    index = i;
+                }
+            }
+            Console.WriteLine($"Namnet som kommer först i bokstavsordning är: {namn[index]}");
+            Console.ReadKey();
+        }
+    }
+}
